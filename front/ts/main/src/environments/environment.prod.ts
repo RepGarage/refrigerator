b184084e21fb37a8d693a2f2f53636cd850fa649
Unknown <goncharovnikita.dev@gmail.com>
2017-12-03 17:53:49 +0300

    Fix prod env url front main

--- front/ts/main/src/environments/environment.prod.ts
+++ front/ts/main/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { firebaseSecret } from './.secret';
 
 export const environment = {
   production: true,
-  productsBaseApi: 'http://ref-back-main/',
+  productsBaseApi: 'http://ref-back-main:8081/',
   firebaseSecret
 };
