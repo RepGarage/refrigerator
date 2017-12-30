d4d72b5369bbb5d352e86673ef6e43189820789e
Unknown <goncharovnikita.dev@gmail.com>
2017-12-30 18:52:38 +0300

    Add interface abstractions to db

--- back/go/main/db/errors.go
+++ back/go/main/db/errors.go
@@ -6,3 +6,10 @@ type UnknownModelError struct{}
 func (e UnknownModelError) Error() string {
 	return "unknown model type passed"
 }
+
+// UnknownDatabaseNameTypeError type
+type UnknownDatabaseNameTypeError struct{}
+
+func (e UnknownDatabaseNameTypeError) Error() string {
+	return "unknown database name type passed"
+}
